2b394f137cf4f95e32fccac86f6c33cf5e884ef1
Leonardo Gabriel <leonardog_da_silva@hotmail.com>
2023-02-19 11:23:55 +0000

    First Problem

--- src/main/java/com/interview/shoppingbasket/Basket.java
+++ src/main/java/com/interview/shoppingbasket/Basket.java
@@ -1,7 +1,9 @@
 package com.interview.shoppingbasket;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Basket {
     private List<BasketItem> items = new ArrayList<>();
@@ -20,6 +22,16 @@ public class Basket {
     }
 
     public void consolidateItems() {
-        // Exercise - implement this function
+        Map<String, BasketItem> consolidateMap = new LinkedHashMap<>();
+        
+        for (BasketItem basketItem : items) {
+			if(consolidateMap.containsKey(basketItem.getProductCode())) {
+				BasketItem itemToSumQuantity = consolidateMap.get(basketItem.getProductCode());
+				itemToSumQuantity.setQuantity(itemToSumQuantity.getQuantity() + basketItem.getQuantity());
+			} else {
+				consolidateMap.put(basketItem.getProductCode(), basketItem);
+			}
+		}
+        items = new ArrayList<BasketItem>(consolidateMap.values());
     }
 }
